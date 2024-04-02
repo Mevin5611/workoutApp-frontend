98be1a8677caafb0b05f84d1d674929fe3485d32
Mevin5611 <mevinmevi88@gmail.com>
2024-04-02 15:09:59 +0530

    vercel deploy success

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('api/workouts/',{
+            const response = await fetch('https://workout-app-backend-dun.vercel.app/api/workouts/',{
                 headers:{
                     'Authorization':`Bearer ${user.token}`
                 }
